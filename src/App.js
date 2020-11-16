2d85d07a6a76ae2d930f58842a4c3852b3dd104c
Yash Gupta <yashg160@gmail.com>
2020-11-16 18:30:28 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./Login/Login";
 import "antd/dist/antd.css";
 
 function App() {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Login} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
