796c1bc2c86fb708540f0ca225e6f112ba1438a0
Yash Gupta <yashg160@gmail.com>
2020-11-16 23:08:21 +0530

    Redux store config

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import Login from "./Login/Login";
 import Dashboard from "./Dashboard/dashboard";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/store";
 
 function App() {
+	const store = ConfigureStore();
+	console.log(store);
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Login} />
-				<Route exact path="/dashboard" component={Dashboard} />
-			</Switch>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route exact path="/dashboard" component={Dashboard} />
+				</Switch>
+			</Router>
+		</Provider>
 	);
 }
 
