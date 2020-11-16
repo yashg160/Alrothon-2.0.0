df8d0eb21551aeaaa95ba25817b41fb0366e2aef
Sumit <sumitbhagat329@gmail.com>
2020-11-16 21:41:07 +0545

    Cleaned Code

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
 var mongoose = require('mongoose');
 
-// var indexRouter = require('./routes/index');
 var powerRouter = require('./routes/power');
-// var programRouter = require('./routes/program');
-// var teacherRouter = require('./routes/teacher');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -30,7 +25,7 @@ mongoose.connect('mongodb+srv://sumit:sumitbhagat@cluster0.1pfkx.mongodb.net/alb
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-// mongoose.connect('mongodb://localhost:27017/routine', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/albenero', { useNewUrlParser: true });
 var db = mongoose.connection;
 
 
@@ -45,11 +40,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
 app.use('/power', powerRouter);
 
-// app.use('/api/program', programRouter);
-// app.use('/api/teacher', teacherRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -57,14 +49,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// client.connect(function (err) {
-//   assert.equal(null, err);
-//   console.log("Connected successfully to server");
 
-//   const db = client.db(dbName);
-
-//   client.close();
-// });
 
 // error handler
 app.use(function (err, req, res, next) {
