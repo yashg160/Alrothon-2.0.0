9344904852513e7d79036419bd3af3237d046e44
Yash Gupta <yashg160@gmail.com>
2020-11-16 18:36:32 +0530

    History prop for routing

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,81 +1,79 @@
 import React, { useState } from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 8,
-  },
+	labelCol: {
+		span: 8,
+	},
+	wrapperCol: {
+		span: 8,
+	},
 };
 const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 16,
-  },
+	wrapperCol: {
+		offset: 8,
+		span: 16,
+	},
 };
 
-const Login = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const onFinish = (values) => {
-    console.log("Success:", values);
-    setName(values.username);
-    setPassword(values.password);
-    console.log(name, password);
-  };
+const Login = ({ history }) => {
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
+	const onFinish = (values) => {
+		console.log("Success:", values);
+		setName(values.username);
+		setPassword(values.password);
+		console.log(name, password);
+		history.push("/dashboard");
+	};
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+	const onFinishFailed = (errorInfo) => {
+		console.log("Failed:", errorInfo);
+	};
 
-  return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+	return (
+		<Form
+			{...layout}
+			name="basic"
+			initialValues={{
+				remember: true,
+			}}
+			onFinish={onFinish}
+			onFinishFailed={onFinishFailed}>
+			<Form.Item
+				label="Username"
+				name="username"
+				rules={[
+					{
+						required: true,
+						message: "Please input your username!",
+					},
+				]}>
+				<Input />
+			</Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+			<Form.Item
+				label="Password"
+				name="password"
+				rules={[
+					{
+						required: true,
+						message: "Please input your password!",
+					},
+				]}>
+				<Input.Password />
+			</Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+			<Form.Item {...tailLayout} name="remember" valuePropName="checked">
+				<Checkbox>Remember me</Checkbox>
+			</Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  );
+			<Form.Item {...tailLayout}>
+				<Button type="primary" htmlType="submit">
+					Submit
+				</Button>
+			</Form.Item>
+		</Form>
+	);
 };
 
 export default Login;
