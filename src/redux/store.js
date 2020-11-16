36911c8fda09b477d75a2d92519a3ba1a5a47ca4
Yash Gupta <yashg160@gmail.com>
2020-11-16 23:08:44 +0530

    Store config for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import Usage from "./Usage";
 
 const rootReducer = combineReducers({
+	usage: Usage,
 	/* global: Global,
 	country: Country,
 	india: India, */
