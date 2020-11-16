ec0fb10ba67e3d77edb71b882722d6034a318a59
Yash Gupta <yashg160@gmail.com>
2020-11-17 00:13:48 +0530

    Updated to use redux

--- src/Threshold/Threshold.js
+++ src/Threshold/Threshold.js
@@ -1,38 +1,45 @@
 import React, { Component } from "react";
 import { Card, InputNumber, Button } from "antd";
 import "./Threshold.css";
+import { updateThreshold } from "../redux/ActionCreators";
+import { connect } from "react-redux";
 class Threshold extends Component {
-  state = {
-    ThresholdValue: "",
-  };
-  onChange(value) {
-    console.log("changed", value);
-  }
+	constructor(props) {
+		super(props);
+	}
+	onChange(value) {
+		this.props.updateThreshold(value);
+	}
 
-  render() {
-    return (
-      <div className="Threshold-center">
-        <div className="site-card-wrapper">
-          <Card title="Threshold Power Consumption" bordered={false}>
-            <div className="Value-inline">
-              <p>Enter The New Threshold Value</p>
-              <InputNumber
-                min={0}
-                max={100}
-                defaultValue={0}
-                onChange={this.onChange}
-              />
-            </div>
-            <br />
+	render() {
+		console.log(this.props);
+		return (
+			<div className="Threshold-center">
+				<div className="site-card-wrapper">
+					<Card title="Threshold Power Consumption" bordered={false}>
+						<div className="Value-inline">
+							<p>Enter The New Threshold Value</p>
+							<InputNumber
+								min={0}
+								max={100}
+								defaultValue={this.props.threshold.threshold}
+								onChange={(value) => this.onChange(value)}
+							/>
+						</div>
+						<br />
 
-            <Button type="primary" shape="" htmlType="submit">
-              UPDATE
-            </Button>
-          </Card>
-        </div>
-      </div>
-    );
-  }
+						<Button type="primary" shape="" htmlType="submit">
+							UPDATE
+						</Button>
+					</Card>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Threshold;
+const mapStateToProps = (state) => ({
+	threshold: state.threshold,
+});
+
+export default connect(mapStateToProps, { updateThreshold })(Threshold);
